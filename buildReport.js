ca113546d2659bd91d5386fd4dbeb02c73a37263
RedMurloc <newcrabnew@gmail.com>
2019-06-05 18:02:32 +0300

    text and table. Table doesn't fit the window.

--- buildReport.js
+++ buildReport.js
@@ -188,3 +188,65 @@
       $('#Cnvs6').parent().remove();
     }
   }
+
+  component7.onclick = function() {
+    if(!$(this).hasClass('checked')){
+      $('#report').append('<div class="col-lg-4">\
+          <div id="text1" style="height: 300px; width: 300px; margin-top: 40px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); text-align: center; border: 1px solid black;">\
+            Текст текст текст текст текст текст текст текст текст текст текст текст текст текст текст текст текст МНОГО ТЕКСТА 1234567789009429444444 \
+          </div>');
+
+    }
+    else{
+      $('#text1').parent().remove();
+    }
+  }
+
+  component8.onclick = function() {
+    if(!$(this).hasClass('checked')){
+      $('#report').append('<div class="col-lg-4">\
+          <table id="table1">\
+  <tr>\
+    <th>Company</th>\
+    <th>Contact</th>\
+    <th>Country</th>\
+  </tr>\
+  <tr>\
+    <td>Alfreds Futterkiste</td>\
+    <td>Maria Anders</td>\
+    <td>Germany</td>\
+  </tr>\
+  <tr>\
+    <td>Centro comercial Moctezuma</td>\
+    <td>Francisco Chang</td>\
+    <td>Mexico</td>\
+  </tr>\
+  <tr>\
+    <td>Ernst Handel</td>\
+    <td>Roland Mendel</td>\
+    <td>Austria</td>\
+  </tr>\
+  <tr>\
+    <td>Island Trading</td>\
+    <td>Helen Bennett</td>\
+    <td>UK</td>\
+  </tr>\
+  <tr>\
+    <td>Laughing Bacchus Winecellars</td>\
+    <td>Yoshi Tannamuri</td>\
+    <td>Canada</td>\
+  </tr>\
+  <tr>\
+    <td>Magazzini Alimentari Riuniti</td>\
+    <td>Giovanni Rovelli</td>\
+    <td>Italy</td>\
+  </tr>\
+</table>');
+
+    }
+    else{
+      $('#table1').parent().remove();
+    }
+  }
+
+  
